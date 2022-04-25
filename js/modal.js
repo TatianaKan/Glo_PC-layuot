ae5ca7dfe28d45769974220a85fdeed2c3482488
TatianaKan <klanukt@gmail.com>
2022-04-25 23:27:41 +0300

    инкапсуляция

--- js/modal.js
+++ js/modal.js
@@ -1,49 +1,53 @@
-const modalBtn = document.querySelector('.modal__button');
-const modal = document.querySelector('.modal');
-const modalBtnCourse = document.querySelector('.course__button');
-// const modalClose = document.querySelector('.modal__close');
+const modal = () => {
+  const modalBtn = document.querySelector('.modal__button');
+  const modal = document.querySelector('.modal');
+  const modalBtnCourse = document.querySelector('.course__button');
+  // const modalClose = document.querySelector('.modal__close');
 
-const modaInner = modal.querySelector('.modal__inner')
-modaInner.style.position = 'relative';
+  const modaInner = modal.querySelector('.modal__inner')
+  modaInner.style.position = 'relative';
 
 
-const addCloseBtn = () => {
-  const closeBtn = document.createElement('div');
+  const addCloseBtn = () => {
+    const closeBtn = document.createElement('div');
 
-  closeBtn.classList.add('close-modal');
-  closeBtn.innerHTML = '&times';
-  closeBtn.style.position = 'absolute';
-  closeBtn.style.right = '10px';
-  closeBtn.style.top = '10px';
-  closeBtn.style.fontSize = '30px';
-  closeBtn.style.cursor = 'pointer';
+    closeBtn.classList.add('close-modal');
+    closeBtn.innerHTML = '&times';
+    closeBtn.style.position = 'absolute';
+    closeBtn.style.right = '10px';
+    closeBtn.style.top = '10px';
+    closeBtn.style.fontSize = '30px';
+    closeBtn.style.cursor = 'pointer';
 
-  modaInner.append(closeBtn);
+    modaInner.append(closeBtn);
 
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = '';
-  });
-}
-
-modalBtn.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  addCloseBtn();
-});
-
-modalBtnCourse.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  addCloseBtn();
-});
-
-// modalClose.addEventListener('click', ()=> {
-//   modal.style.display = '';
-// })
-
-modal.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  const modalContent = event.target.closest('.modal__inner');
-  if (!modalContent) {
-    modal.style.display = '';
+    closeBtn.addEventListener('click', () => {
+      modal.style.display = '';
+    });
   }
-})
+
+  modalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    addCloseBtn();
+  });
+
+  modalBtnCourse.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    addCloseBtn();
+  });
+
+  // modalClose.addEventListener('click', ()=> {
+  //   modal.style.display = '';
+  // })
+
+  modal.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const modalContent = event.target.closest('.modal__inner');
+    if (!modalContent) {
+      modal.style.display = '';
+    }
+  })
+};
+
+modal();
